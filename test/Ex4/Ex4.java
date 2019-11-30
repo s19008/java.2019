838e106188dcb6ff34973b967b48f0b878835695
Yo shinzato <s19008@std.it-college.ac.jp>
2019-11-30 18:54:22 +0900

    second

--- test/Ex4/Ex4.java
+++ test/Ex4/Ex4.java
@@ -1,67 +1,129 @@
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
 
 class Ex4{
 
-	public int [] ex4_1(int [] a){
-		
-		return a;
+    public int [] ex4_1(int [] a){
 
-	}
+        List<Integer> list = new ArrayList<Integer>();
+        for (int tmp : a) {
+            list.add((Integer)tmp);
+        }
 
-	public int [] ex4_2(int [] a){
-		
-		return a;
+        Collections.sort(list);
 
-	}
+        for (int i = 0; i< a.length; i++) {
+            a[i] = list.get(i);
+        }
+        return a;
+    }
 
-	public String [] ex4_3(int [] a){
-		
-		String [] hantei = {"dummy"};
-		
-		return hantei;
+    public int [] ex4_2(int [] a){
 
-	}
 
-	public int ex4_4(int a){
-		
-		return -1;
+        List<Integer> list = new ArrayList<Integer>();
+        for (int tmp : a) {
+            list.add((Integer)tmp);
+        }
 
-	}
+        Collections.sort(list, Comparator.reverseOrder());
 
-	public int ex4_5(int a){
-		
-		return -1;
+        for (int i = 0; i< a.length; i++) {
+            a[i] = list.get(i);
+        }
+        return a;
+    }
 
-	}
+    public String [] ex4_3(int [] a){
 
-	public int [] ex4_6(int a){
-		
-		int [] ret = new int[1];
-		
-		return ret;
+        String [] ret = new String[a.length];
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > 0){
+                ret[i] = "正";
+            } else if (a[i] < 0) {
+                ret[i] = "負";
+            } else {
+                ret[i] = "零";
+            }
+        }
+        return ret;
+    }
 
-	}
+    public int ex4_4(int a){
 
-	public boolean ex4_7(int a){
-		
-		return false;
+        int val = a;
+        int vallen = String.valueOf(val).length();
 
-	}
+        return vallen;
 
-	public int ex4_8(int [] a){
-		
-		int ret = 0;
-		
-		return ret;
+    }
 
-	}
+    public int ex4_5(int a){
 
-	public int ex4_9(int [] a){
-		
-		int ret = 0;
-		
-		return ret;
+        int sum = 0;
+        for (int i = 1; i <= a; i++){
+            sum += i;
+        }
+        return sum;
 
-	}
+    }
+
+    public int [] ex4_6(int a){
+
+        List<Integer> yaku = new ArrayList<Integer>();
+
+        for (int i = 1; i <= a; i++) {
+            if (a % i ==0) {
+                yaku.add(i);
+            }
+        }
+
+        int [] ret = new int[yaku.size()];
+
+        for (int i = 0; i < ret.length; i++) {
+            ret[i] = yaku.get(i);
+        }
+        return ret;
+    }
+
+    public boolean ex4_7(int a){
+        if (a ==1) {
+            return false;
+        }
+
+        for (int i = 2; i < a; i++) {
+            if (a % i ==0) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    public int ex4_8(int [] a){
+
+        int ret = 0;
+        for (int i :a){
+            ret += i;
+        }
+        return ret;
+
+    }
+
+    public int ex4_9(int [] a){
+
+        float sum = 0;
+
+        for (int i : a) {
+            sum += i;
+        }
+
+        int ret = Math.round(sum/a.length);
+
+        return ret;
+
+    }
 }
-
